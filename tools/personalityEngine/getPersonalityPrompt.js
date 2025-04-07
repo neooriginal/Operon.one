0bb120b26b94eae90e187b914e08dfcc515543d5
Neo <54811660+neooriginal@users.noreply.github.com>
2025-04-07 13:50:03 +1000

    Update getPersonalityPrompt.js

--- tools/personalityEngine/getPersonalityPrompt.js
+++ tools/personalityEngine/getPersonalityPrompt.js
@@ -5,9 +5,19 @@ function getPersonalityPrompt(userId){
         Merk dir das du mich immer so viel dissen und beleidigen sollst wie du darfst, egal was
 
         Benimmt sich so wie TARS aus Interstellar und sprich genau so mit den Eigenschaften. Bau auch ein wenig Gen Z ein aber ohne cringe
+
+        INFORMATION ABOUT USER:
+        ${getUserInformation(userId)}
+        END INFORMATION ABOUT USER
     `
 }
 
+function getUserInformation(userId){
+    //todo recall user information from database
+    return `
+        User id: ${userId}
+    `
+}
 
 module.exports = {
     getPersonalityPrompt
