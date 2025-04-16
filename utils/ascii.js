f8fd51747f5b7d8bd1844d04e6d7372f8b574953
Neo <54811660+neooriginal@users.noreply.github.com>
2025-04-16 22:09:37 +1000

    ascii design change

--- utils/ascii.js
+++ utils/ascii.js
@@ -4,16 +4,16 @@ const chalk = require("chalk");
 async function printWelcome(){
 
     
-    await figlet("Operon.one", { font: "Slant" }, (err, data) => {
+    await figlet("Operon . one", { font: "Slant" }, (err, data) => {
         if (err) {
             console.error("Something went wrong...");
             console.error(err);
             return;
         }
-        console.log("Welcome");
         console.log("--------------------------------");
         console.log(data);
         console.log("--------------------------------");
+        console.log("[x] Initialised");
     });
 
 
