9dac195bb23b643eb25b671c29dc3ad73ad4c527
Neo <54811660+neooriginal@users.noreply.github.com>
2025-04-27 21:07:32 +1000

    feat: custom database path

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 
 // Ensure the data directory exists
-const DATA_DIR = path.join(__dirname, 'data');
+const DATA_DIR = process.env.DATA_DIR || path.join(__dirname, 'data');
 if (!fs.existsSync(DATA_DIR)) {
   fs.mkdirSync(DATA_DIR, { recursive: true });
 }
